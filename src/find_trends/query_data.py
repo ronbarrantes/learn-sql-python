5f9596a8c1ec0416dcce4d34c04a8aeec29760c6
Ron Barrantes <ronbarrantes@gmail.com>
2026-03-14 12:46:53 -0400

    Update the query_data

--- src/find_trends/query_data.py
+++ src/find_trends/query_data.py
@@ -1,19 +1,24 @@
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
 client = bigquery.Client()
 
 query = """
-    SELECT name, SUM(number) as total_people
-    FROM `bigquery-public-data.usa_names.usa_1910_2013`
-    WHERE state = 'TX'
-    GROUP BY name, state
-    ORDER BY total_people DESC
-    LIMIT 20
-"""
-rows = client.query_and_wait(query)  # Make an API request.
+    SELECT 
+        rank, 
+        dma_name AS Name, 
+        term, 
+        refresh_date AS Day
+    FROM 
+        `bigquery-public-data.google_trends.top_rising_terms`
+    ORDER BY Day Desc
+    LIMIT 50
+  """
 
-print("The query data:")
-for row in rows:
-    # Row values can be accessed by field name or index.
-    print("name={}, count={}".format(row[0], row["total_people"]))
+def the_query():
+    rows = client.query_and_wait(query)
+    print("The query data:")
+    for row in rows:
+        print("name={}, day={}, rank={}".format(row["Name"], row["Day"], row["rank"]))
+
+the_query()
+
